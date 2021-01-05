44cac6d5ff27a901c7172aadd20f4500d0793348
Levi <bickelsolutions@gmail.com>
2021-01-05 15:30:11 -0700

    Add project files.

---
+++ Browser/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Browser
+{
+    partial class BrowserMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonForward = new System.Windows.Forms.Button();
+            this.AddressBar = new System.Windows.Forms.TextBox();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.panelBrowser = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(12, 12);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 27);
+            this.buttonBack.TabIndex = 0;
+            this.buttonBack.Text = "Back";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonForward
+            // 
+            this.buttonForward.Location = new System.Drawing.Point(94, 11);
+            this.buttonForward.Name = "buttonForward";
+            this.buttonForward.Size = new System.Drawing.Size(75, 28);
+            this.buttonForward.TabIndex = 1;
+            this.buttonForward.Text = "Forward";
+            this.buttonForward.UseVisualStyleBackColor = true;
+            this.buttonForward.Click += new System.EventHandler(this.buttonForward_Click);
+            // 
+            // AddressBar
+            // 
+            this.AddressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddressBar.Location = new System.Drawing.Point(176, 12);
+            this.AddressBar.Name = "AddressBar";
+            this.AddressBar.Size = new System.Drawing.Size(494, 22);
+            this.AddressBar.TabIndex = 2;
+            this.AddressBar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AddressBar_KeyPress);
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGo.Location = new System.Drawing.Point(677, 11);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(94, 28);
+            this.buttonGo.TabIndex = 3;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // panelBrowser
+            // 
+            this.panelBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelBrowser.AutoSize = true;
+            this.panelBrowser.Location = new System.Drawing.Point(12, 41);
+            this.panelBrowser.Name = "panelBrowser";
+            this.panelBrowser.Size = new System.Drawing.Size(776, 397);
+            this.panelBrowser.TabIndex = 4;
+            // 
+            // BrowserMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelBrowser);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.AddressBar);
+            this.Controls.Add(this.buttonForward);
+            this.Controls.Add(this.buttonBack);
+            this.Name = "BrowserMain";
+            this.Text = "Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonForward;
+        private System.Windows.Forms.TextBox AddressBar;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.Panel panelBrowser;
+    }
+}
+
---
+++ Browser/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace Browser
+{
+    public partial class BrowserMain : Form
+    {
+        public BrowserMain()
+        {
+            InitializeComponent();
+            InitChromium();
+        }
+        public ChromiumWebBrowser c;
+
+        public void InitChromium()
+        {
+            Cef.EnableHighDPISupport();
+            Cef.Initialize(new CefSettings());
+            c = new ChromiumWebBrowser("https://www.google.com");
+            c.AddressChanged += C_AddressChanged;
+            c.TitleChanged += C_TitleChanged;
+            panelBrowser.Controls.Add(c);
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void C_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            this.Text = e.Title;
+        }
+
+        private void C_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            AddressBar.Text = e.Address.ToString();
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            c.Back();
+        }
+
+        private void buttonForward_Click(object sender, EventArgs e)
+        {
+            c.Forward();
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            c.Load(AddressBar.Text);
+        }
+
+        private void AddressBar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                buttonGo_Click(null, null);
+            }
+            
+        }
+    }
+}
