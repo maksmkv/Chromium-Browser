1a059ca86e9b97f5b6996e8a5c60b8942fadba4d
Levi <bickelsolutions@gmail.com>
2021-01-11 13:36:45 -0700

    Removed comment on the appContainer.

--- Browser/AppContainer.cs
+++ Browser/AppContainer.cs
@@ -15,8 +15,6 @@ namespace Browser
         }
         static AppContainer()
         {
-            // This is only so that generating a thumbnail for Aero peek works properly:  with GPU acceleration enabled, all you get is a black box
-            // when you try to "snapshot" the web browser control.  If you don't plan on using Aero peek, remove this method.
             CefSettings cefSettings = new CefSettings();
             cefSettings.DisableGpuAcceleration();
 
