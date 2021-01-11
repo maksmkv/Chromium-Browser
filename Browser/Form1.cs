307531bb05ffa9c2c5bde570ad149dbe4173c381
Levi <bickelsolutions@gmail.com>
2021-01-11 13:19:23 -0700

    Currently appears that this function is not supported with CEFSharp. The context menu is a baked in function currently.

--- Browser/Form1.cs
+++ Browser/Form1.cs
@@ -27,7 +27,7 @@ namespace Browser
             {
                 _tab = tab;
             }
-
+            
             public bool DoClose(IWebBrowser chromiumWebBrowser, IBrowser browser)
             {
                 return true;
@@ -85,7 +85,6 @@ namespace Browser
                         };
                     }
                 }));
-
                 newBrowser = null;
                 return true;
             }
@@ -112,7 +111,6 @@ namespace Browser
             InitializeComponent();
 
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-
             if(address == null || address == "")
             {
                 WebBrowser = new ChromiumWebBrowser("about:blank")
@@ -139,8 +137,6 @@ namespace Browser
                     LifeSpanHandler = new NewTabLifespanHandler(this)
                 };
             }
-
-            
             Controls.Add(WebBrowser);
             WebBrowser.TitleChanged += WebBrowser_TitleChanged;
             WebBrowser.AddressChanged += WebBrowser_AddressChanged;
@@ -256,7 +252,7 @@ namespace Browser
                 {
                     fullUrl = "http://" + fullUrl;
                 }
-
+                e.SuppressKeyPress = true;
                 faviconLoaded = false;
                 WebBrowser.Load(fullUrl);
             }
@@ -284,113 +280,8 @@ namespace Browser
 
         private void BrowserMain_FormClosing(object sender, FormClosingEventArgs e)
         {
-
+            Cef.Shutdown();
+            this.Close();
         }
     }
 }
-//    public partial class BrowserMain : Form
-//    {
-//        public BrowserMain()
-//        {
-//            InitializeComponent();
-//        }
-//        public ChromiumWebBrowser c;
-
-//        public void InitChromium()
-//        {
-//            Cef.EnableHighDPISupport();
-//            c = new ChromiumWebBrowser("https://www.google.com") {
-//                Anchor = (AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom),
-//                Location = new Point(0, 38),
-//                MinimumSize = new Size(20, 20),
-//                Size = new Size(326, 251),
-//                Name = "Browser"
-//            };
-//            c.Dock = DockStyle.Fill;
-//            c.AddressChanged += C_AddressChanged;
-//            c.TitleChanged += C_TitleChanged;
-//            panelBrowser.Controls.Add(c);
-//            CheckForIllegalCrossThreadCalls = false;
-//        }
-
-//        private void C_TitleChanged(object sender, TitleChangedEventArgs e)
-//        {
-//            this.Text = e.Title;
-//        }
-
-//        private void C_AddressChanged(object sender, AddressChangedEventArgs e)
-//        {
-//            AddressBar.Text = e.Address;
-//        }
-
-//        private void buttonBack_Click(object sender, EventArgs e)
-//        {
-//            if (c.CanGoBack)
-//            {
-//                c.Back();
-//            }
-//        }
-
-//        private void buttonForward_Click(object sender, EventArgs e)
-//        {
-//            if (c.CanGoForward)
-//            {
-//                c.Forward();
-//            }
-//        }
-
-//        private void buttonGo_Click(object sender, EventArgs e)
-//        {
-//            if (AddressBar.Text.Contains("https://www") || AddressBar.Text.Contains("http://www"))
-//            {
-//                c.Load(AddressBar.Text);
-//            }
-//            else if (!AddressBar.Text.Contains("www"))
-//            {
-//                if (AddressBar.Text.Contains("https://") || AddressBar.Text.Contains("http://"))
-//                {
-//                    AddressBar.Text = AddressBar.Text.Substring(8);
-//                    AddressBar.Text = "https://www." + AddressBar.Text;
-//                    c.Load(AddressBar.Text);
-//                }
-//                else if (!AddressBar.Text.Contains(".com"))
-//                {
-//                    c.Load("https://duckduckgo.com/?q=" + AddressBar.Text);
-//                }
-//                else
-//                {
-//                    c.Load("https://www." + AddressBar.Text);
-//                }
-//            }
-//            else {
-//                c.Load("https://" + AddressBar.Text);
-//            }
-//        }
-
-//        private void AddressBar_KeyPress(object sender, KeyPressEventArgs e)
-//        {
-//            if(e.KeyChar == (char)Keys.Enter)
-//            {
-//                e.Handled = true;
-//                buttonGo_Click(null, null);
-//            }
-
-//        }
-
-//        private void buttonRefresh_Click(object sender, EventArgs e)
-//        {
-//            c.Refresh();
-
-//        }
-
-//        private void BrowserMain_Load(object sender, EventArgs e)
-//        {
-//            InitChromium();
-//        }
-
-//        private void BrowserMain_FormClosing(object sender, FormClosingEventArgs e)
-//        {
-//            Cef.Shutdown();
-//        }
-//    }
-//}
