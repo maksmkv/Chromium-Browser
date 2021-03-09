a6d92c442552b699c151a928215e75a74ef35439
Levi <bickelsolutions@gmail.com>
2021-03-09 09:34:23 -0500

    Added usersettings to store homepage and search preferences. Will need to fix the menu visible settings to mimick the bookmarks tab.

--- Browser/Program.cs
+++ Browser/Program.cs
@@ -1,10 +1,5 @@
-﻿using CefSharp;
-using CefSharp.WinForms;
-using EasyTabs;
+﻿using EasyTabs;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Browser
